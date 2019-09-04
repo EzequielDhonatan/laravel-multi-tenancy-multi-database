88f037e4a48ad0df83229aee4697aee13a4333aa
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-09-04 10:43:30 -0300

    Configuration and Translation - Laravel 6.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>Laravel Multi Tenancy Multi Database</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,600"> <!-- Fonts -->
 
         <!-- Styles -->
         <style>
@@ -64,37 +62,45 @@
         </style>
     </head>
     <body>
+
         <div class="flex-center position-ref full-height">
+
             @if (Route::has('login'))
+
                 <div class="top-right links">
+                    
                     @auth
+
                         <a href="{{ url('/home') }}">Home</a>
+
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+
+                        <a href="{{ route('login') }}">Entrar</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Cadastro</a>
                         @endif
+
                     @endauth
-                </div>
+
+                </div> <!-- top-right links -->
+
             @endif
 
             <div class="content">
+
                 <div class="title m-b-md">
-                    Laravel
+                    <p>Laravel Multi Tenancy Multi Database</p>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.linkedin.com/in/ezequieldhonatan/">LinkedIn</a>
+                    <a href="https://github.com/ezequieldhonatan/laravel-multi-tenancy-multi-database">GitHub</a>
                 </div>
-            </div>
-        </div>
+
+            </div> <!-- content -->
+
+        </div> <!-- flex-center position-ref full-height -->
+
     </body>
 </html>
